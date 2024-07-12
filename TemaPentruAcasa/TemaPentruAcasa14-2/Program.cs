69bbae140288ebf635458a2e6eb2dc02189cba8d
anastasiaCazacu <anastasia.cazacu@gmail.com>
2024-07-12 08:34:16 +0300

    Problema nr2

---
+++ TemaPentruAcasa/TemaPentruAcasa14-2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TemaPentruAcasa14_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
