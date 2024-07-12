88310872fef2a9609efdfec61f0e69e633b9d468
anastasiaCazacu <anastasia.cazacu@gmail.com>
2024-07-12 17:24:13 +0300

    Unique code generation. Tema15.1

---
+++ TemaPentruAcasa/Problema15.1/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Problema15._1
+{
+    public static class Helper
+    {
+        private static Random random = new Random();
+        public static string GenerareCodUnic(string prefix)
+        {
+            int numberRandomN = random.Next(100, 999);
+            int numberRandomY = random.Next(1000, 9999);
+            return prefix + numberRandomN + numberRandomY;      
+        }
+    }
+}
---
+++ TemaPentruAcasa/Problema15.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Problema15._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------GenerareCodUnic-------------");
+
+            string cod1 = Helper.GenerareCodUnic("NCI");
+            Console.WriteLine(cod1 + "\n");
+
+            string cod2 = Helper.GenerareCodUnic("TRX");
+            Console.WriteLine(cod2 + "\n");
+
+            string cod3 = Helper.GenerareCodUnic("TRX");
+            Console.WriteLine(cod3 + "\n");
+        }
+    }
+}
